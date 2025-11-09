f3fe49af0b6b6a4809e2c00b6b74f03f08c5a5ad
Brian Reynolds <brian@brianreynolds.net>
2025-11-08 22:09:50 -0500

    1.4.1 - Clean up the journal text

--- YouSpotALegendary.cs
+++ YouSpotALegendary.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 using Qud.API;
 using Qud.UI;
@@ -30,77 +31,31 @@ namespace XRL.World.Parts
                     bool result = base.Render(E); // Render before we indicate
                     ParentObject.Indicate(ParentObject.IsHostileTowards(The.Player));
                     
-                    String spotted = "You spot " + ParentObject.DisplayName + ".";
+                    String spotted = "You spot " + ParentObject.DisplayName + ".\n";
 
-                    String factionString = "";
-                    List<string> parentFactions = new List<string>(2);
                     var givesRep = ParentObject.GetPart<GivesRep>();
-                    foreach (var faction in givesRep.GetLovedFactions(VisibleOnly: true, WithoutReason: true))
-                    {
-                        parentFactions.Add(faction.Name);
-                    }
-
-                    bool any = false;
-                    if (parentFactions.Count > 0)
-                    {
-                        foreach (var member in parentFactions)
-                        {
-                            if (any)
-                            {
-                                factionString += ", Loved/Member: ";
-                            }
-                            factionString += member;
-                            any = true;
-                        }
-                    }
+                    StringBuilder SB = new();
+                    givesRep.AppendReputationDescription(SB);
                     
-                    foreach (FriendorFoe f in givesRep.relatedFactions)
-                    {
-                        Faction faction = Factions.GetIfExists(f.faction);
-                        if (faction is { Visible: true } && !f.reason.IsNullOrEmpty())
-                        {
-                            if (any)
-                            {
-                                factionString += "; ";
-                            }
-
-                            if (f.status == "love")
-                            {
-                                factionString += "Loved: " + faction.Name;
-                            }
-
-                            if (f.status == "friend")
-                            {
-                                factionString += "Admired: " + faction.Name;
-                            }
-                            else if (f.status == "dislike")
-                            {
-                                factionString += "Disliked: " + faction.Name;
-                            }
-                            else if (f.status == "hate")
-                            {
-                                factionString += "Hated: " + faction.Name;
-                            }
-
-                            any = true;
-                        }
-                    }
-                    
+                    var repString = SB.ToString();
                     if (AutoAct.Setting != "")
                     {
-                        XRL.Messages.MessageQueue.AddPlayerMessage(spotted + ((factionString != "") ? " (" + factionString + ")" : ""));
-                        int ichoice = Popup.PickOption(Title: "", Intro: spotted,
+                        XRL.Messages.MessageQueue.AddPlayerMessage(spotted + repString);
+                        int ichoice = Popup.PickOption(Title: "", Intro: spotted + repString + "\n",
                             Options: new string[] { "Continue", "Interrupt Autoexplore" });
                         if (ichoice == 1) AutoAct.Interrupt();
                     }
                     else
                     {
-                        Qud.API.IBaseJournalEntry.DisplayMessage(spotted + ((factionString != "") ? " (" + factionString  + ")" : ""));
+                        Qud.API.IBaseJournalEntry.DisplayMessage(spotted + repString);
                     }
 
                     if (Options.GetOptionBool("OptionYouSpotALegendaryAddJournalEntry"))
                     {
-                        JournalAPI.AddMapNote(The.Player.CurrentZone.ZoneID, factionString, "Miscellaneous", null, null,
+                        repString = repString.Replace("\n", " ");
+                        repString = repString.Replace("{{C|-----}}", "");
+                        
+                        JournalAPI.AddMapNote(The.Player.CurrentZone.ZoneID, ParentObject.DisplayName + ": " + repString, "Miscellaneous", null, null,
                             true, true);
                     }
 
